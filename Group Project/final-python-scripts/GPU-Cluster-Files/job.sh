56d1d8bc6faf099883a37b2006940f7dcde7f50c
Kartikeya <KARTIKEY001@e.ntu.edu.sg>
2021-11-15 19:43:02 +0800

    Add files via upload

---
+++ Group Project/final-python-scripts/GPU-Cluster-Files/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name=CZ4042
+#SBATCH --output=Output.out
+#SBATCH --error=Error.err
+#SBATCH --nodes=1
+#SBATCH --partition=SCSEGPU_UG
+#SBATCH --mem=8000M
+#SBATCH --gres=gpu:1
+
+module load anaconda
+source activate /apps/conda_env/CZ4042_v3
+python main.py
